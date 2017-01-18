e9d648a8a060fa0574a082f26351b2a22258d9bf
solome <iliyang.cn@gmail.com>
2017-01-18 23:40:49 +0800

    `render` => `controllers`

--- test/app-proto/server/config.js
+++ test/app-proto/server/config.js
@@ -5,7 +5,7 @@ const logger = console.log
 
 logger(join(__dirname, 'context'))
 logger(join(__dirname, 'middlewares'))
-logger(join(__dirname, 'render'))
+logger(join(__dirname, 'controllers'))
 logger(join(__dirname, 'templates'))
 logger(join(__dirname, 'datasources'))
 logger(join(__dirname, '..', 'client', 'pages'))
@@ -14,7 +14,7 @@ export default {
   path: {
     context: join(__dirname, 'context'),
     middlewares: join(__dirname, 'middlewares'),
-    render: join(__dirname, 'render'),
+    render: join(__dirname, 'controllers'),
     templates: join(__dirname, 'templates'),
     datasources: join(__dirname, 'datasources'),
     pages: join(__dirname, '..', 'client', 'pages'),
