0dfdd0578d5ec60dadafdfa852b6937385c03530
solome <iliyang.cn@gmail.com>
2017-06-05 20:06:40 +0800

    update

--- explore/index.js
+++ explore/index.js
@@ -10,4 +10,14 @@ app.use(async (ctx, next) => {
   log(ctx)
 })
 
+
+import path from 'path'
+import glob from 'glob'
+
+const join = path.join
+
+glob(join('**/*.js'), { cwd: '../src', dot: false, sync: true })
+   .forEach(file => console.log(file))
+
+
 app.listen(3000)
